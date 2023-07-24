044829954f4f4c8b35e53803bfc3be7fe5e41ce7
VerduciLuca <135212650+VerduciLuca@users.noreply.github.com>
2023-07-24 13:32:01 +0200

    trfetr

--- app.js
+++ app.js
@@ -80,23 +80,23 @@ function fire4(){
 var colors = ['#bb0000', '#ffffff'];
 
 (function frame() {
-  confetti({
-    particleCount: 2,
-    angle: 60,
-    spread: 55,
-    origin: { x: 0 },
-    colors: colors
-  });
-  confetti({
-    particleCount: 2,
-    angle: 120,
-    spread: 55,
-    origin: { x: 1 },
-    colors: colors
-  });
+    confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+    });
+    confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
+    });
 
-  if (Date.now() < end) {
+    if (Date.now() < end) {
     requestAnimationFrame(frame);
-  }
+    }
 }());
 }
